fa5d461705e58241c776f1b208f6234832be961d
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-03-05 16:23:59 +0100

    added all the code from other .tsx files to  App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import './App.css'
-import {useState} from "react";
+import React, {useState} from "react";
 import {Character, charactersResponse} from "./characters.ts";
-import CharacterGallery from "./CharacterGallery.tsx";
+import {Routes, Route} from "react-router-dom";
+import HomeWillkommen from "./assets/HomeWillkommen.tsx";
+import Characters from "./assets/characters.tsx";
+import HeaderPage from "./assets/headerPage.tsx";
+import CharacterRoute from "./assets/CharacterRoute.tsx";
 
 function App() {
 
     const [characters, setCharacters] = useState<Character[]>(charactersResponse.results)
 
     return (
-        <>
-            <CharacterGallery  characters={characters}/>
-        </>
+        <div>
+
+            <Routes>
+                <Route path ="/Home/Willkommen" element = {<HomeWillkommen /> } />
+                <Route path = "/characters" element = {<Characters /> } />
+                <Route path = "/headerPage" element= {<HeaderPage /> } />
+                <Route path = "/characters/:id" element = {< CharacterRoute />} />
+            </Routes>
+
+        </div>
     )
 }
 
