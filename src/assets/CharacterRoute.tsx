0f7eacf8dc55c723fc4812cc0f4b547edb949394
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-03-06 17:11:33 +0100

    added code to CharacterRoute.tsx

--- src/assets/CharacterRoute.tsx
+++ src/assets/CharacterRoute.tsx
@@ -1,6 +1,6 @@
-
 import {useParams} from "react-router-dom";
 import {charactersResponse} from "../characters.ts";
+
 export default function CharacterRoute() {
     const params = useParams();
     const id: string | undefined = params.id;
@@ -9,9 +9,9 @@ export default function CharacterRoute() {
             {charactersResponse.results
                 .filter(c => c.id.toString() === id)
                 .map(c => <p>
-                        {c.name},
+                    {c.name},
                     {c.species}
-                    </p>)}
+                </p>)}
         </div>
     )
 }
